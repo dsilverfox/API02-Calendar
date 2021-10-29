927f44d4ccaeed94d18900f736f9c9573eadf06d
Devon Silverfox-Patchett <DE.Silverfox.Coding@gmail.com>
2021-10-29 04:04:45 -0400

    Initial Commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const url = "https://api.nasa.gov/planetary/apod";
+const apiKey = "?api_key=1ISSVszznkytKzI3tUJcvLzk1EfijTyQ3Li6GFSI";
+//Additions
+const start = "&start_date=";
+const end = "&end_date=";
+const count = "&count=";
+//Dates must be entered as YYYY-MM-DD
+
+
